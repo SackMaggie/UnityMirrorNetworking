a315b2930629a1b3f81830737b7dc5ec2e55381b
SackMaggie <athpon@live.com>
2021-02-01 09:33:10 +0700

    Initial comit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : NetworkBehaviour
+{
+    public static NetworkEvent onDataReceive = new NetworkEvent();
+    public class NetworkEvent : UnityEvent<DataCommand, object[]> { };
+    public static Player selfPlayer;
+
+    public static void SendData(DataCommand command, object[] data)
+    {
+        if (selfPlayer == null)
+            selfPlayer = NetworkClient.connection.identity.GetComponent<Player>();
+        selfPlayer.CmdSend(command, data);
+    }
+
+    [Command]
+    public void CmdSend(DataCommand command, object[] data)
+    {
+        RpcReceive(command, data);
+    }
+
+    [ClientRpc]
+    public void RpcReceive(DataCommand command, object[] data)
+    {
+        onDataReceive.Invoke(command, data);
+    }
+}
+
+public enum DataCommand
+{
+    TEST1
+}
---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEST : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.onDataReceive.AddListener(OnDataReceive);
+        button.onClick.AddListener(OnClicked);
+    }
+
+    private void OnClicked()
+    {
+        Player.SendData(DataCommand.TEST1, null);
+    }
+
+    private void OnDataReceive(DataCommand arg0, object[] arg1)
+    {
+        Debug.Log(arg0.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
