110faa0355f9d2a95d2f1af44668e82fd12cb751
CLe4N <64629943+CLe4N@users.noreply.github.com>
2021-02-02 16:26:53 +0700

    Waypoint Instantiate

---
+++ Assets/Scripts/monopoly/CreateWaypoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWaypoint : MonoBehaviour
+{
+    public GameObject waypointPre;
+    Vector2 BordPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(waypointPre, this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
