750b92c856069a090d4405d23784e9a918757910
CLe4N <64629943+CLe4N@users.noreply.github.com>
2021-02-02 13:14:56 +0700

    dice

---
+++ Assets/Scripts/RNGDice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RNGDice : MonoBehaviour
+{
+    public Button btt;
+    public TMPro.TextMeshProUGUI diceText;
+
+    private void Start()
+    {
+        btt.onClick.AddListener(randomNum);
+    }
+
+    private void randomNum()
+    {
+        diceText.text = Random.Range(1, 7).ToString();
+    }
+}
