4157fbd46bd7901f526edad9b0072ff54f08b43d
CLe4N <64629943+CLe4N@users.noreply.github.com>
2021-02-02 16:28:07 +0700

    Waypoint Instantiate

--- Assets/Scripts/monopoly/CreateWaypoint.cs
+++ Assets/Scripts/monopoly/CreateWaypoint.cs
@@ -6,10 +6,42 @@ public class CreateWaypoint : MonoBehaviour
 {
     public GameObject waypointPre;
     Vector2 BordPos;
+    float posX = 43;
+    float posY = 43;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(waypointPre, this.transform);
+        for (int i = 0; i <= 32; i++)
+        {
+            if (i < 8)
+            {
+                GameObject waypoint = Instantiate(waypointPre, this.transform);
+                waypoint.transform.localPosition = new Vector3(posX, -posY);
+                waypoint.name = "waypoint" + i.ToString();
+                posX = posX - 10.75f; ;
+            }
+            else if (i > 8 && i <= 16)
+            {
+                GameObject waypoint = Instantiate(waypointPre, this.transform);
+                waypoint.transform.localPosition = new Vector3(posX, -posY);
+                waypoint.name = "waypoint" + i.ToString();
+                posY = posY - 10.75f; ;
+            }
+            else if (i > 16 && i <= 24)
+            {
+                GameObject waypoint = Instantiate(waypointPre, this.transform);
+                waypoint.transform.localPosition = new Vector3(posX, -posY);
+                waypoint.name = "waypoint" + i.ToString();
+                posX = posX + 10.75f; ;
+            }
+            else if (i > 24)
+            {
+                GameObject waypoint = Instantiate(waypointPre, this.transform);
+                waypoint.transform.localPosition = new Vector3(posX, -posY);
+                waypoint.name = "waypoint" + i.ToString();
+                posY = posY + 10.75f; ;
+            }
+        }
     }
 
     // Update is called once per frame
